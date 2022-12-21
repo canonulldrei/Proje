baa74a99c0ddf6a93a5e7b304f3880351100ac02
Can <115797202+canonulldrei@users.noreply.github.com>
2022-12-21 20:49:49 +0300

    Arkaplanla birlikte revizeler yapıldı.

--- sim.py
+++ sim.py
@@ -1,6 +1,7 @@
 import pygame as pg 
 import sys
 pg.init()
+clock = pg.time.Clock()
 
 en , boy = 1200 , 800 #Ekran Büyüklügü
 
@@ -8,13 +9,13 @@ en , boy = 1200 , 800 #Ekran Büyüklügü
 siyah =  0 , 0 , 0
 beyaz = 255 , 255 , 255
 gri = 125 , 125 , 125
-yesil = 29 , 117 , 53
+yesil = 61 , 252 , 3
 k_mavi = 5 , 7 , 54
 sari = 209 , 209 , 46
 
 #Boyut ve arkaplan
 ekran = pg.display.set_mode( (en , boy ) )
-arkaplan = bg = pg.image.load("background.jfif")
+arkaplan = bg = pg.image.load("background.jpg")
 
 while True:
     for event in pg.event.get():
@@ -24,9 +25,25 @@ while True:
     #ekran.fill(beyaz)
     ekran.blit(pg.transform.scale (bg, (1200,800)), (0,0))
 
-#Ekrandaki çizimler
+### Tüm Oyun Mantığı Buranın Altına Yazılacak
+
+
+
+
+
+
+
+
+
+#Buraya da Ekrandaki Çizimler Girilecek
     #pg.draw.rect(ekran, k_mavi, (250, 550, 500, 12), 6)              #Denge Cubugu
 
 
 
+
+
+
+
+
     pg.display.flip()
+    clock.tick(20) #Fps ayarlamak için
