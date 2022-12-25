1625a3247a5a6e6c3b7ad9d15aad304a90f261bb
Can <115797202+canonulldrei@users.noreply.github.com>
2022-12-25 19:19:28 +0300

    Birtakım Değişiklikler yapıldı.

--- sim.py
+++ sim.py
@@ -1,9 +1,15 @@
 import pygame as pg 
 import sys
+
+#Pygame'i baslatmak
 pg.init()
 clock = pg.time.Clock()
 
-en , boy = 1200 , 800 #Ekran Büyüklügü
+#Ekran ölcüleri
+en , boy = 1200 , 800
+
+#FPS ayari
+FPS = 30
 
 #Renkler
 siyah =  0 , 0 , 0
@@ -16,18 +22,26 @@ sari = 209 , 209 , 46
 #Boyut ve arkaplan
 ekran = pg.display.set_mode( (en , boy ) )
 pg.display.set_caption("Tork ve Denge Simülasyonu")
-arkaplan = bg = pg.image.load("background.jpg")
-#Tahterevalli
-stick = st = pg.image.load("stick.png")
-taban =tb =pg.image.load("taban.png")
-#Agirliklar
-kettlebell_5 = kb_5 =pg.image.load("kettlebell_5.png")
-kettlebell_10 = kb_10 =pg.image.load("kettlebell_10.png")
-kettlebell_20 = kb_20 =pg.image.load("kettlebell_20.png")
+arkaplan = bg = pg.image.load("img/background.jpg")
 
+#Tahterevalli
+stick = st = pg.image.load("img/stick.png")
+st.convert()
+taban =tb =pg.image.load("img/taban.png")
+tb.convert()
+#Agirliklar
+kettlebell_5 = kb_5 =pg.image.load("img/kettlebell_5.png")
+kb_5.convert()
+kettlebell_10 = kb_10 =pg.image.load("img/kettlebell_10.png")
+kb_10.convert()
+kettlebell_20 = kb_20 =pg.image.load("img/kettlebell_20.png")
+kb_20.convert()
+
+#Oyun Döngüsü
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT :
+            run=False
             pg.quit()
             sys.exit();
 
@@ -39,21 +53,27 @@ while True:
     ekran.blit(pg.transform.scale (kb_5,(38,51)), (1000,80))
     ekran.blit(pg.transform.scale (kb_10,(38,51)), (1050,80))
     ekran.blit(pg.transform.scale (kb_20,(38,51)), (1100,80))
+    pg.display.update()
 
-### Tüm Oyun Mantığı Buranın Altına Yazılacak
+### Tüm Oyun Mantığı Buranın Altına Yazılacak ###
 
 #Tork hesaplama
-def torque():     #Burada temel hesaplamamiz icin tork = kuvvet*kuvvet kolu  bagintisi girilmeli
-    
-
-#Tahterevalliyi hareket ettirme ve üzerinde belli uzakliklar tanimlayabilme
+#def torque():     #Burada temel hesaplamamiz icin tork = kuvvet*kuvvet kolu  bagintisi girilmeli
 
 
 
-#Agirliklari mouse ile sürükleme
+#Tahterevalliyi orta noktasindan hareket ettirme ve üzerinde belli uzakliklar tanimlayabilme
+
+
+
+
+
+######Agirliklari mouse ile sürükleme
+
 
 
 #Tahterevalliyi üzerine binen agirliklara göre hareket ettirme
+
 
 
 #Agirliklari tahterevalli üzerinde yerlestirme ve isleme alma
@@ -64,5 +84,6 @@ def torque():     #Burada temel hesaplamamiz icin tork = kuvvet*kuvvet kolu  bag
 ####Buraya da Ekrandaki Çizimler Girilecek
 
 
-    pg.display.flip()
-    clock.tick(20) #Fps ayarlamak için
+
+clock.tick(FPS)
+pg.display.flip()
