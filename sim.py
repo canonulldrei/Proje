385c675063855deeb5436077486641d3ef96d54b
Abdallamedhat <116125384+Abdallamedhat@users.noreply.github.com>
2023-01-18 22:30:03 +0300

    Basıldığında hareketli ağırlıklar ekledim

--- sim.py
+++ sim.py
@@ -98,101 +98,87 @@ class Butonlar():
             is_clicked_kg3 = False
             is_clicked_kg4 = False
             is_clicked_kg5 = False
-            is_clicked_kg6 = False
+            is_clicked_kg6 = False 
     
-    
+    # Basıldığında "kg1" hareket ettir
+
+    if is_clicked_kg1:
+        mouse_pos = pg.mouse.get_pos()
+        kg1_rect.x = mouse_pos[0] - kg1_rect.en/2
+        kg1_rect.y = mouse_pos[1] - kg1_rect.boy/2
+
+    # Basıldığında "kg2" hareket ettir
+
+     if is_clicked_kg2:
+            mouse_pos = pg.mouse.get_pos()
+        kg2_rect.x = mouse_pos[0] - kg2_rect.en/2
+        kg2_rect.y = mouse_pos[1] - kg2_rect.boy/2
+
+    # Basıldığında "kg3" hareket ettir
+
+     if is_clicked_kg3:
+            mouse_pos = pg.mouse.get_pos()
+        kg3_rect.x = mouse_pos[0] - kg3_rect.en/2
+        kg3_rect.y = mouse_pos[1] - kg3_rect.boy/2
                             
+    # Basıldığında "kg4" hareket ettir
 
-# Butonlari atama
-start_btn = Butonlar(950, 150, start_img, 1)
-restart_btn = Butonlar(1050, 150, restart_img, 1)
+     if is_clicked_kg4:
+            mouse_pos = pg.mouse.get_pos()
+        kg4_rect.x = mouse_pos[0] - kg4_rect.en/2
+        kg4_rect.y = mouse_pos[1] - kg4_rect.boy/2
 
-#Boyut ve arkaplan
-en , boy = 1200 , 700
-ekran = pg.display.set_mode( (en , boy) )
-pg.display.set_caption("Tork ve Denge Simülasyonu")
-pg.display.set_icon(pg.image.load("img/kettlebell_5.png"))
-arkaplan = pg.image.load("img/background.png")
-taban = pg.image.load("img/ground.png")
+    # Basıldığında "kg5" hareket ettir
 
-#Tahterevalli
-tahterevalli = pg.image.load("img/stick.png").convert_alpha()
-tahterevalli_rect = tahterevalli.get_rect()
-tahterevalli_rect.x = 100
-tahterevalli_rect.y = 540
+     if is_clicked_kg5:
+            mouse_pos = pg.mouse.get_pos()
+        kg5_rect.x = mouse_pos[0] - kg5_rect.en/2
+        kg5_rect.y = mouse_pos[1] - kg5_rect.boy/2
 
+    # Basıldığında "kg6" hareket ettir
 
-
- #Agirliklar
-  # Birinci 5 Kg
-kg1 = pg.image.load("img/kettlebell_5.png")
-kg1_rect = kg1.get_rect()
-kg1_rect.x = 800
-kg1_rect.y = 80
-is_clicked_kg1 = False
-
-  # Ikinci 5 Kg
-kg2 = pg.image.load("img/kettlebell_5.png")
-kg2_rect = kg2.get_rect()
-kg2_rect.x = 850
-kg2_rect.y = 80
-is_clicked_kg2 = False
-
-  # Birinci 10 Kg
-kg3 = pg.image.load("img/kettlebell_10.png")
-kg3_rect = kg3.get_rect()
-kg3_rect.y = 900
-kg3_rect.y = 80
-is_clicked_kg3 = False
-
-  # Ikinci 10 Kg
-kg4 = pg.image.load("img/kettelebell_10.png")
-kg4_rect = kg4.get_rect()
-kg4_rect.x = 950
-kg4_rect.y = 80
-is_clicked_kg4 = False
-
-  # Birinci 20 Kg
-kg5 = pg.image.load("img/kettlebell_20.png")
-kg5_rect = kg5.get_rect()
-kg5_rect.x = 1000
-kg5_rect.y = 80
-is_clicked_kg5 = False
-
-  # Ikinci 20 Kg
-kg6 = pg.image.load("img/kettlebell_20.png")
-kg6_rect = kg6.get_rect()
-kg6_rect.x = 1050
-kg6_rect.y = 80
-is_clicked_kg6 = False
-
-
-#Oyun Döngüsü
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT :
-            run=False
-            pg.quit()
-            sys.exit();
-
-
-#Ekranda cikacak ögeler
-    ekran.blit(tahterevalli, tahterevalli_rect)
-    ekran.blit(taban, (520,565) )
-    # ekran.blit(pg.transform.scale (kb_5,(38,51)), (900,80))
-    # ekran.blit(pg.transform.scale (kb_5,(38,51)), (950,80))
-    # ekran.blit(pg.transform.scale (kb_10,(38,51)), (1000,80))
-    # ekran.blit(pg.transform.scale (kb_10,(38,51)), (1050,80))
-    # ekran.blit(pg.transform.scale (kb_20,(38,51)), (1100,80))
-    # ekran.blit(pg.transform.scale (kb_20,(38,51)), (1150,80))
-
-    #En asagida olacak
-    pg.display.update()
-
-### Tüm Oyun Mantığı Buranın Altına Yazılacak ###
+     if is_clicked_kg6:
+            mouse_pos = pg.mouse.get_pos()
+        kg6_rect.x = mouse_pos[0] - kg6_rect.en/2
+        kg6_rect.y = mouse_pos[1] - kg6_rect.boy/2
+ 
 
 
 
 
-#Tork hesaplama
-#Tahterevalliyi orta noktasindan hareket ettirme ve üzerinde belli uzakliklar taniml
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+clock.tick(FPS)
+pg.display.flip()
