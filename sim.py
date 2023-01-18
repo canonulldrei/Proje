342805a3720cebe7f142aa10a625719c55cadfae
Can <115797202+canonulldrei@users.noreply.github.com>
2023-01-18 20:21:07 +0300

    agirliklari tanimlarken degisiklikler yapildii

--- sim.py
+++ sim.py
@@ -75,13 +75,49 @@ tahterevalli_rect.y = 540
 
 
 
-#Agirliklar
-kettlebell_5 = kb_5 =pg.image.load("img/kettlebell_5.png")
-kb_5.convert()
-kettlebell_10 = kb_10 =pg.image.load("img/kettlebell_10.png")
-kb_10.convert()
-kettlebell_20 = kb_20 =pg.image.load("img/kettlebell_20.png")
-kb_20.convert()
+ #Agirliklar
+  # Birinci 5 Kg
+kg1 = pg.image.load("img/kettlebell_5.png")
+kg1_rect = kg1.get_rect()
+kg1_rect.x = 800
+kg1_rect.y = 80
+is_clicked_kg1 = False
+
+  # Ikinci 5 Kg
+kg2 = pg.image.load("img/kettlebell_5.png")
+kg2_rect = kg2.get_rect()
+kg2_rect.x = 850
+kg2_rect.y = 80
+is_clicked_kg2 = False
+
+  # Birinci 10 Kg
+kg3 = pg.image.load("img/kettlebell_10.png")
+kg3_rect = kg3.get_rect()
+kg3_rect.y = 900
+kg3_rect.y = 80
+is_clicked_kg3 = False
+
+  # Ikinci 10 Kg
+kg4 = pg.image.load("img/kettelebell_10.png")
+kg4_rect = kg4.get_rect()
+kg4_rect.x = 950
+kg4_rect.y = 80
+is_clicked_kg4 = False
+
+  # Birinci 20 Kg
+kg5 = pg.image.load("img/kettlebell_20.png")
+kg5_rect = kg5.get_rect()
+kg5_rect.x = 1000
+kg5_rect.y = 80
+is_clicked_kg5 = False
+
+  # Ikinci 20 Kg
+kg6 = pg.image.load("img/kettlebell_20.png")
+kg6_rect = kg6.get_rect()
+kg6_rect.x = 1050
+kg6_rect.y = 80
+is_clicked_kg6 = False
+
 
 #Oyun Döngüsü
 while True:
@@ -95,12 +131,12 @@ while True:
 #Ekranda cikacak ögeler
     ekran.blit(tahterevalli, tahterevalli_rect)
     ekran.blit(taban, (520,565) )
-    ekran.blit(pg.transform.scale (kb_5,(38,51)), (900,80))
-    ekran.blit(pg.transform.scale (kb_5,(38,51)), (950,80))
-    ekran.blit(pg.transform.scale (kb_10,(38,51)), (1000,80))
-    ekran.blit(pg.transform.scale (kb_10,(38,51)), (1050,80))
-    ekran.blit(pg.transform.scale (kb_20,(38,51)), (1100,80))
-    ekran.blit(pg.transform.scale (kb_20,(38,51)), (1150,80))
+    # ekran.blit(pg.transform.scale (kb_5,(38,51)), (900,80))
+    # ekran.blit(pg.transform.scale (kb_5,(38,51)), (950,80))
+    # ekran.blit(pg.transform.scale (kb_10,(38,51)), (1000,80))
+    # ekran.blit(pg.transform.scale (kb_10,(38,51)), (1050,80))
+    # ekran.blit(pg.transform.scale (kb_20,(38,51)), (1100,80))
+    # ekran.blit(pg.transform.scale (kb_20,(38,51)), (1150,80))
 
     #En asagida olacak
     pg.display.update()
